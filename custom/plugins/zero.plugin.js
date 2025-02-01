24be524066e039f6244ae926de8d1d1dd64a5647
TerraZero <terrazerocore@gmail.com>
2025-02-01 12:41:48 +0100

    update session

--- custom/plugins/zero.plugin.js
+++ custom/plugins/zero.plugin.js
@@ -2,7 +2,9 @@ import RemoteSystem from 'zero-system/src/RemoteSystem';
 import Client from 'zero-system/src/Nuxt/Socket/Client';
 
 import namespace from '../namespaces/remote.namespace';
+import Logger from 'zero-system/src/Log/Logger';
 
+Logger.setDebug(true);
 export default new RemoteSystem(new Client(), namespace);
 
 /**
